0ff95f03f8d78e75af846902abc4783059c2474b
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-05-12 20:07:24 +0530

    true

--- src/components/MyApproachCanvas .jsx
+++ src/components/MyApproachCanvas .jsx
@@ -57,7 +57,7 @@ const Card = ({ title, icon,description, children }) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      onClick={() => setHovered(!hovered)}
+      onClick={() => setHovered(true)}
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 relative lg:h-[30rem] rounded-3xl"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
