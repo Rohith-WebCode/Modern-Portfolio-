76ea7423135cba4f2cf62f11aa0e18e06395fa8f
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-05-25 11:02:34 +0530

    debugging

--- src/components/ui/Bento-grid.jsx
+++ src/components/ui/Bento-grid.jsx
@@ -46,7 +46,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "group/bento  shadow-input row-span-1 flex flex-col justify-between  space-y-4  relative overflow-hidden rounded-3xl border border-red-500 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento row-span-1 flex flex-col justify-between  space-y-4  relative overflow-hidden rounded-3xl border   p-4 transition duration-200 hover:shadow-xl border-white/[0.2] bg-black shadow-none",
         className
       )}>
       <div className="transition duration-200 group-hover/bento:translate-x-2">
